ba7ef7269edc81131912ccca70239fd925277803
IsraelCost <cmteisrael@gmail.com>
2022-07-03 12:20:35 -0300

    test: ensure DBAddUser calls add user repository with correct data

--- tests/application/usecases/db-add-user.spec.ts
+++ tests/application/usecases/db-add-user.spec.ts
@@ -1,28 +1,44 @@
 import { DBAddUser } from '@/application/usecases'
-import { DBAddUserDTO } from '@/application/dto/db-add-user'
+import { DBAddUserDTO } from '@/application/dto'
+import { AddUserRepository } from '@/application/protocols'
+import { User } from '@/domain/models/user'
 
 interface SutTypes {
   sut: DBAddUser
+  addUserRepositoryStub: AddUserRepository
+}
+
+const makeAddUserRepositoryStub = (): AddUserRepository => {
+  class AddUserRepositoryStub implements AddUserRepository {
+    async add (input: User): Promise<User> {
+      return Promise.resolve(null)
+    }
+  }
+  return new AddUserRepositoryStub()
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new DBAddUser()
+  const addUserRepositoryStub = makeAddUserRepositoryStub()
+  const sut = new DBAddUser(addUserRepositoryStub)
   return {
-    sut
+    sut,
+    addUserRepositoryStub
   }
 }
 
 const makeFakerDTO = (): DBAddUserDTO.Input => {
   return {
+    name: 'any_name',
     email: 'any_mail@mail.com',
-    password: 'any_password'
+    password: 'any_password',
+    profilePictureUrl: 'any_profile_picture_url'
   }
 }
 
 describe('DBAddUser usecase', () => {
-  test('Should call add method with correct DTO', async () => {
-    const { sut } = makeSut()
-    const addSpy = jest.spyOn(sut, 'add')
+  test('Should call addUserRepository method with user', async () => {
+    const { sut, addUserRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addUserRepositoryStub, 'add')
     const user = makeFakerDTO()
     await sut.add(user)
     expect(addSpy).toHaveBeenCalledWith(user)
