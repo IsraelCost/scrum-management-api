d9cf4cc8ec183b186306bb88d4848789133f5e86
IsraelCost <cmteisrael@gmail.com>
2022-07-03 18:53:55 -0300

    test: ensure adapter returns the hashed value

--- tests/infra/cryptography/hasher.spec.ts
+++ tests/infra/cryptography/hasher.spec.ts
@@ -10,4 +10,11 @@ describe('HasherAdapter', () => {
     sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', 10)
   })
+
+  test('Should return bcrypt hashed value', () => {
+    const sut = new HasherAdapter(10)
+    jest.spyOn(bcrypt, 'hashSync').mockReturnValueOnce('hashed_value')
+    const hashedValue = sut.hash('any_value')
+    expect(hashedValue).toBe('hashed_value')
+  })
 })
