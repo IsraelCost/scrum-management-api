34cad3e7b3feeff61fd94fe1c2f747e44c814d4e
IsraelCost <cmteisrael@gmail.com>
2022-07-03 13:26:07 -0300

    feat: ensure DBAddUser calls hasher with correct password

--- src/application/protocols/index.ts
+++ src/application/protocols/index.ts
@@ -1,2 +1,3 @@
 export * from './add-user-repository'
 export * from './check-user-exists-repository'
+export * from './hasher'
--- src/application/usecases/db-add-user.ts
+++ src/application/usecases/db-add-user.ts
@@ -1,16 +1,18 @@
 import { DBAddUserDTO } from '../dto'
 import { UserAlreadyExistsError } from '../errors'
-import { AddUserRepository, CheckUserExistsRepository } from '../protocols'
+import { AddUserRepository, CheckUserExistsRepository, Hasher } from '../protocols'
 
 export class DBAddUser {
   constructor (
     private readonly addUserRepository: AddUserRepository,
-    private readonly checkUserExistsRepository: CheckUserExistsRepository
+    private readonly checkUserExistsRepository: CheckUserExistsRepository,
+    private readonly hasher: Hasher
   ) {}
 
   async add (input: DBAddUserDTO.Input): Promise<DBAddUserDTO.Output> {
     const userExists = await this.checkUserExistsRepository.exists(input.email)
     if (userExists) throw new UserAlreadyExistsError()
+    this.hasher.hash(input.password)
     await this.addUserRepository.add(input)
     return Promise.resolve(null)
   }
