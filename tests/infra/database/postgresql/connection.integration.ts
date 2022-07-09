66c37b4918e0d031872044527e8704416feeab3c
IsraelCost <cmteisrael@gmail.com>
2022-07-09 17:35:13 -0300

    test: ensure PostgreSQLConnection connects correctly

---
+++ tests/infra/database/postgresql/connection.integration.ts
@@ -0,0 +1,25 @@
+import { PostgreSQLConnection } from '@/infra/database/postgresql/connection'
+
+describe('PostgreSQLConnection', () => {
+  const OLD_ENV = process.env
+
+  beforeAll(() => {
+    process.env = {
+      DB_USER: 'postgres',
+      DB_HOST: 'localhost',
+      DB_NAME: 'scrum-management',
+      DB_PASSWORD: '123456',
+      DB_PORT: '5432'
+    } 
+  })
+
+  afterAll(() => {
+    process.env = OLD_ENV
+  })
+
+  test('Should create client and connect to server', async () => {
+    const sut = new PostgreSQLConnection()
+    const connected = await sut.open()
+    expect(connected).toBeTruthy()
+  })
+})
