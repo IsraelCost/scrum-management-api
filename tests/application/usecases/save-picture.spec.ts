7c325b79e0eb158230196f6a4c8bbeb64f041641
IsraelCost <cmteisrael@gmail.com>
2022-07-12 23:18:34 -0300

    test: ensure calls and throws uuid generator

--- tests/application/usecases/save-picture.spec.ts
+++ tests/application/usecases/save-picture.spec.ts
@@ -1,10 +1,20 @@
-import { ImageUploader } from '@/application/protocols'
+import { ImageUploader, UUIDGenerator } from '@/application/protocols'
 import { SavePictureUsecase } from '@/domain/usecases'
 import { SavePicture } from '@/application/usecases/save-picture'
 
 interface SutTypes {
   sut: SavePictureUsecase
   imageUploaderStub: ImageUploader
+  uuidGeneratorStub: UUIDGenerator
+}
+
+const makeUUIDGeneratorStub = (): UUIDGenerator => {
+  class UUIDGeneratorStub implements UUIDGenerator {
+    generate (): string {
+      return 'uuid'
+    }
+  }
+  return new UUIDGeneratorStub()
 }
 
 const makeImageUploaderStub = (): ImageUploader => {
@@ -17,15 +27,36 @@ const makeImageUploaderStub = (): ImageUploader => {
 }
 
 const makeSut = (): SutTypes => {
+  const uuidGeneratorStub = makeUUIDGeneratorStub()
   const imageUploaderStub = makeImageUploaderStub()
-  const sut = new SavePicture(imageUploaderStub)
+  const sut = new SavePicture(imageUploaderStub, uuidGeneratorStub)
   return {
     sut,
-    imageUploaderStub
+    imageUploaderStub,
+    uuidGeneratorStub
   }
 }
 
 describe('SavePicture', () => {
+  test('Should call uuid generator', async () => {
+    const { sut, uuidGeneratorStub } = makeSut()
+    const generateSpy = jest.spyOn(uuidGeneratorStub, 'generate')
+    await sut.save('image_path', Buffer.from(''))
+    expect(generateSpy).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should throws if uuid generator throws', async () => {
+    const { sut, uuidGeneratorStub } = makeSut()
+    jest.spyOn(uuidGeneratorStub, 'generate').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    try {
+      await sut.save('image_path', Buffer.from(''))
+    } catch (error) {
+      expect(error).toEqual(new Error())
+    }
+  })
+
   test('Should call image uploader with correct values', async () => {
     const { sut, imageUploaderStub } = makeSut()
     const uploadSpy = jest.spyOn(imageUploaderStub, 'upload')
