99455e8d46a5b8d20536dbe105620cb576843585
IsraelCost <cmteisrael@gmail.com>
2022-07-12 23:18:03 -0300

    feat: ensure calls and throws uuid generator

--- src/application/usecases/save-picture.ts
+++ src/application/usecases/save-picture.ts
@@ -1,10 +1,14 @@
 import { SavePictureUsecase } from '@/domain/usecases'
-import { ImageUploader } from '../protocols'
+import { ImageUploader, UUIDGenerator } from '../protocols'
 
 export class SavePicture implements SavePictureUsecase {
-  constructor (private readonly imageUploader: ImageUploader) {}
+  constructor (
+    private readonly imageUploader: ImageUploader,
+    private readonly uuidGenerator: UUIDGenerator
+  ) {}
 
   async save (path: string, buffer: Buffer): Promise<string> {
+    this.uuidGenerator.generate()
     const imageLocation = await this.imageUploader.upload(path, buffer)
     return imageLocation
   }
