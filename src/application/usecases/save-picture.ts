d347b9a85fdd9f341dabfffa3c3f2afc1af9471d
IsraelCost <cmteisrael@gmail.com>
2022-07-12 22:54:24 -0300

    feat: ensure save picture usecase returns image location

--- src/application/usecases/save-picture.ts
+++ src/application/usecases/save-picture.ts
@@ -5,7 +5,7 @@ export class SavePicture implements SavePictureUsecase {
   constructor (private readonly imageUploader: ImageUploader) {}
 
   async save (path: string, buffer: Buffer): Promise<string> {
-    this.imageUploader.upload(path, buffer)
-    return Promise.resolve(null)
+    const imageLocation = await this.imageUploader.upload(path, buffer)
+    return imageLocation
   }
 }
